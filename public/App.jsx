2387e0efa2640c335285de54c9b32c938a2ebcda
Sitanshu Pokalwar <sitz2309@gmail.com>
2022-09-10 17:48:51 +0530

    🎨 structure change

---
+++ public/App.jsx
@@ -0,0 +1,12 @@
+import "../src/App.css";
+
+function App() {
+    return (
+        <div className=''>
+            <h1 className='font-med'>Hello world!</h1>
+
+        </div>
+    );
+}
+
+export default App;
